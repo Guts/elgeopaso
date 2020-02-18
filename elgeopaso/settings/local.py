c0a88986c660703d9e4593be231d55448fb7d268
Guts <Guts@users.noreply.github.com>
2020-02-18 14:11:48 +0100

    Improve USE_DOCKER option detection

--- elgeopaso/settings/local.py
+++ elgeopaso/settings/local.py
@@ -49,7 +49,8 @@ DEBUG_TOOLBAR_CONFIG = {
 }
 # https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#internal-ips
 INTERNAL_IPS = ["127.0.0.1", "10.0.2.2"]
-if env("USE_DOCKER") == "yes":
+
+if env.bool("USE_DOCKER", default=False):
     import socket
 
     hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
