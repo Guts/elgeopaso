4f1342fd19d145d40786893c4e9c24118f832100
Guts <Guts@users.noreply.github.com>
2020-02-18 14:05:54 +0100

    Handle specific URLs settings in debug mode

--- elgeopaso/urls.py
+++ elgeopaso/urls.py
@@ -1,4 +1,5 @@
-from django.urls import path, include
+from django.conf import settings
+from django.urls import include, path
 
 from django.contrib import admin
 
@@ -19,3 +20,31 @@ urlpatterns = [
     path("db/", jobs.views.db, name="db"),
     path("admin/", admin.site.urls),
 ]
+
+
+# Addtionnal configuration for local development
+if settings.DEBUG:
+    # This allows the error pages to be debugged during development, just visit
+    # these url in browser to see how these error pages look like.
+    # urlpatterns += [
+    #     path(
+    #         "400/",
+    #         default_views.bad_request,
+    #         kwargs={"exception": Exception("Bad Request!")},
+    #     ),
+    #     path(
+    #         "403/",
+    #         default_views.permission_denied,
+    #         kwargs={"exception": Exception("Permission Denied")},
+    #     ),
+    #     path(
+    #         "404/",
+    #         default_views.page_not_found,
+    #         kwargs={"exception": Exception("Page not Found")},
+    #     ),
+    #     path("500/", default_views.server_error),
+    # ]
+    if "debug_toolbar" in settings.INSTALLED_APPS:
+        import debug_toolbar
+
+        urlpatterns = [path("__debug__/", include(debug_toolbar.urls))] + urlpatterns
