cc3e63b0f64a63a508dd915c8f30406d38f0f606
Guts <Guts@users.noreply.github.com>
2020-02-20 15:38:24 +0100

    Restore Accounts and API apps

---
+++ accounts/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+
+from .models import Subscription
+
+
+# USERS
+class SubscriptionInline(admin.StackedInline):
+    model = Subscription
+    can_delete = False
+    verbose_name_plural = "Options"
+
+
+# Define a new User admin
+class UserAdmin(BaseUserAdmin):
+    inlines = (SubscriptionInline,)
+
+
+# REGISTERING and DISPLAY
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
