3042d8275352ee150f610fbf33418dfe0ca6d722
Guts <Guts@users.noreply.github.com>
2020-03-02 22:14:42 +0100

    Load jobs fixtures

--- .deploy/release-tasks.sh
+++ .deploy/release-tasks.sh
@@ -5,6 +5,7 @@ python manage.py migrate
 
 # load fixtures
 python manage.py loaddata ./jobs/fixtures/contracts.json
+python manage.py loaddata ./jobs/fixtures/jobs.json
 python manage.py loaddata ./jobs/fixtures/places.json
 python manage.py loaddata ./jobs/fixtures/technos.json
 python manage.py loaddata ./jobs/fixtures/sources.json
