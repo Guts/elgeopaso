2e36fc68c62ca75a407841e61c1ad7551cbd7b6a
Guts <Guts@users.noreply.github.com>
2020-03-03 11:26:16 +0100

    Load variables form env file at startup

--- manage.py
+++ manage.py
@@ -1,11 +1,86 @@
-#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+#! python3  # noqa: E265
+
 """Django's command-line utility for administrative tasks."""
-import os
+
+# ############################################################################
+# ########## Libraries #############
+# ##################################
+
+# Standard library
+import logging
 import sys
+from os import environ, path
+from pathlib import Path
+
+# 3rd party
+from dotenv import find_dotenv, load_dotenv
+
+# ############################################################################
+# ########## Globals ###############
+# ##################################
+
+start_dir = Path(".")
+
+# look for `.env` files
+dotenv_files = list(start_dir.glob("*.env"))
+if not len(dotenv_files):
+    logging.info(
+        "No environment ('.env') file found in: {}"
+        " Using environment variables stored into the user and system levels.".format(
+            start_dir.resolve()
+        )
+    )
+elif len(dotenv_files) == 1:
+    logging.info("Environment file found: {}".format(dotenv_files[0].resolve()))
+    if dotenv_files[0].name == "example.env":
+        logging.error(
+            "Example environment file should not be used because it's git tracked."
+        )
+        raise
+    # load environment variables
+    load_dotenv(find_dotenv(dotenv_files[0]), override=True)
+else:
+    logging.warning(
+        "Multiple ({}) environment files found. Picking one among: {}".format(
+            len(dotenv_files), str(dotenv_files)
+        )
+    )
+    for env_file in dotenv_files:
+        if env_file.name == ".env":
+            logging.info("Priority given to the pure '.env' file. Using it.")
+            # load environment variables
+            load_dotenv(env_file, override=True)
+            break
+        else:
+            logging.warning(
+                "No specific environment file found. Using the first: {}".format(
+                    dotenv_files[0]
+                )
+            )
+            # load environment variables
+            load_dotenv(dotenv_files[0], override=True)
 
 
+# ############################################################################
+# ######### Functions ##############
+# ##################################
 def main():
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "elgeopaso.settings.local")
+    """Launch the Django's command-line utility for administrative tasks.
+
+    :raises ImportError: if Django is not installed
+
+    :example:
+
+    .. code-block:: shell
+
+        python manage.py --help
+    """
+    # ensure that the Django project settings module to use is in the environment variables.
+    # If not, it'll use the local settings by default
+    environ.setdefault("DJANGO_SETTINGS_MODULE", "elgeopaso.settings.local")
+
+    # then import Django CLI
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -16,12 +91,15 @@ def main():
         ) from exc
 
     # This allows easy placement of apps within the interior project directory.
-    current_path = os.path.dirname(os.path.abspath(__file__))
-    sys.path.append(os.path.join(current_path, "elgeopaso"))
+    current_path = path.dirname(path.abspath(__file__))
+    sys.path.append(path.join(current_path, "elgeopaso"))
 
     # launch cli
     execute_from_command_line(sys.argv)
 
 
+# ############################################################################
+# ######## Stand-alone #############
+# ##################################
 if __name__ == "__main__":
     main()
