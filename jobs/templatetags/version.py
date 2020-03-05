55186e6e9b2e95702fb22c1429df7d9d0e8edc38
Julien <Guts@users.noreply.github.com>
2020-03-05 18:28:25 +0100

    Add project version in footer using a custom templatetag

---
+++ jobs/templatetags/version.py
@@ -0,0 +1,55 @@
+# -*- coding: UTF-8 -*-
+#! python3  # noqa: E265
+
+"""
+    Application views.
+
+    Learn more here: https://docs.djangoproject.com/fr/2.2/topics/http/views/
+"""
+
+# ###########################################################################
+# ######### Libraries #############
+# #################################
+
+# Standard library
+import time
+
+# Django
+from django import template
+from django.conf import settings
+
+# ###########################################################################
+# ########## Globals ##############
+# #################################
+register = template.Library()
+
+# ###########################################################################
+# ####### Template tags ###########
+# #################################
+@register.simple_tag
+def version_date(date_format: str = "%d/%m/%Y") -> str:
+    git_folder = settings.ROOT_DIR / ".git"
+    return time.strftime(date_format, time.gmtime(git_folder.stat().st_mtime))
+
+
+@register.simple_tag
+def version_number() -> str:
+    """Return the project version as number.
+    
+    :return: version number
+    :rtype: str
+
+    :example:
+    
+    .. code-block:: html
+    
+        # from a Django template, first load the custom templatetag
+        {% load version %}
+
+        # in a text div
+        <p>
+          My awesome project - version {% version_number %}
+        </p>
+
+    """
+    return settings.PROJECT_VERSION
