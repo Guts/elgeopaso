08881f54d778737d61a372ecccf63cbd0f307247
Julien <Guts@users.noreply.github.com>
2020-03-12 10:06:45 +0100

    Test against bad XMLs

--- tests/test_crawler_georezo_rss.py
+++ tests/test_crawler_georezo_rss.py
@@ -17,6 +17,7 @@
 # Standard library
 import unittest
 from pathlib import Path
+from sys import _getframe
 
 # 3rd party
 from validator_collection import validators
@@ -29,8 +30,14 @@ from jobs.crawlers import GeorezoRssParser
 # ######## Globals #################
 # ##################################
 
-# variables
+
 extension_pattern = "**/*.xml"
+
+
+def get_test_marker():
+    """Returns the module + function name to get a discriminator value."""
+    return "{}__{}".format(Path(__file__).stem.upper(), _getframe(1).f_code.co_name)
+
 
 # #############################################################################
 # ########## Classes ###############
@@ -51,8 +58,8 @@ class TestCrawlerGeorezo(unittest.TestCase):
     def tearDown(self):
         """Executed after each test."""
         # clean fixtures
-        for tmp_file in self.tmp_fixtures_dir.iterdir():
-            tmp_file.unlink()
+        # for tmp_file in self.tmp_fixtures_dir.iterdir():
+        #     tmp_file.unlink()
 
     # -- TESTS ---------------------------------------------------------
     def test_georezo_parser(self):
@@ -66,8 +73,8 @@ class TestCrawlerGeorezo(unittest.TestCase):
         )
 
         # enforce different metadata file path to avoid conflicts between tests and real process
-        georezo_parser.CRAWLER_LATEST_METADATA = (
-            "tests/fixtures/tmp/crawler_georezo_rss_latest_test.json"
+        georezo_parser.CRAWLER_LATEST_METADATA = "tests/fixtures/tmp/{}.json".format(
+            get_test_marker()
         )
 
         # check
@@ -80,9 +87,25 @@ class TestCrawlerGeorezo(unittest.TestCase):
         # parse
 
     def test_load_rss(self):
-        """Parse RSS sample"""
+        """Parse RSS samples."""
         for i in self.li_fixtures_repo:
-            print(i.resolve())
+            georezo_parser = GeorezoRssParser(
+                feed_base_url=str(i.resolve()),
+                items_to_parse=None,
+                feed_length_param=None,
+                user_agent="ElGeoPaso/{} https://elgeopaso.georezo.net/".format(
+                    __version__
+                ),
+            )
+
+            # enforce different metadata file path to avoid conflicts between tests and real process
+            georezo_parser.CRAWLER_LATEST_METADATA = (
+                "tests/fixtures/tmp/{}_{}.json".format(get_test_marker(), i.stem)
+            )
+
+        # parse feed and retrive new offers
+        li_new_offers_to_add = georezo_parser.parse_new_offers()
+        self.assertIsInstance(li_new_offers_to_add, list)
 
     def test_id_extraction(self):
         """Extract ID from entry"""
@@ -95,11 +118,6 @@ class TestCrawlerGeorezo(unittest.TestCase):
         for raw_str in li_raw_str:
             offer_id = GeorezoRssParser.extract_offer_id_from_url(raw_str)
             self.assertIsInstance(offer_id, int)
-            # self.assertTrue(offer_id.isdigit())
-
-        # using fixtures
-        # for i in self.li_fixtures_repo:
-        #     print(i.resolve())
 
 
 # ##############################################################################
