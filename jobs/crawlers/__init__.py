1f81c0ef47f5c97e21a88356e0ab689c409a8a2f
Guts <Guts@users.noreply.github.com>
2020-03-12 15:02:24 +0100

    Improve folder structure reducing tree

--- jobs/crawlers/__init__.py
+++ jobs/crawlers/__init__.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 #! python3  # noqa: E265
 
-from .georezo import GeorezoRssParser  # noqa: F401
+from .georezo_rss_parser import GeorezoRssParser  # noqa: F401
--- jobs/crawlers/georezo/__init__.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-#! python3  # noqa: E265
-
-from .georezo_rss_parser import GeorezoRssParser  # noqa: F401
