d17c5c81cd22ec68b7052a49b6a0bade0173e0ee
Julien <Guts@users.noreply.github.com>
2020-05-03 15:04:17 +0200

    Remove former code moved to submodule

--- elgeopaso/jobs/analyzer/georezo/georezo_analyzer.py
+++ elgeopaso/jobs/analyzer/georezo/georezo_analyzer.py
@@ -27,17 +27,17 @@ from nltk.corpus import stopwords
 # project modules
 from elgeopaso.jobs.models import (
     Contract,
-    ContractVariations,
     GeorezoRSS,
     JobPosition,
     JobPositionVariations,
     Offer,
     Place,
-    PlaceVariations,
     Source,
     Technology,
     TechnologyVariations,
 )
+
+from .parsers import ContentParser, TitleParser
 
 # ##############################################################################
 # ########## Globals ###############
@@ -106,11 +106,16 @@ class GeorezoOfferAnalizer:
                 pass
             # get raw offer from georezo_rss table
             raw_offer = GeorezoRSS.objects.get(id_rss=offer_id)
-            # clean title
+
+            # -- Title analisis ----------------------
             clean_title = self.remove_html_markups(raw_offer.title)
+            title_parser = TitleParser(clean_title)
+
             # determine contract type
-            contract_type = self.parse_contract_type(clean_title)
-            place = self.parse_place(clean_title, mode=0)
+            contract_type = title_parser.parse_contract_type()
+            place = title_parser.parse_place(mode=0)
+
+            # -- Content analisis ----------------------
             # clean content
             clean_content = self.remove_html_markups(raw_offer.content)
             # content cleaning and nltk tokenizing
@@ -169,118 +174,6 @@ class GeorezoOfferAnalizer:
             logging.debug("Offer analyzed and inserted jobs.offer: {}".format(offer_id))
 
     # PARSERS ----------------------------------------------------------------
-
-    def parse_contract_type(self, offer_clean_title: str):
-        """
-        Extraction of types of contracts: CDI, CDD, mission, volontariat, etc.
-        In theory, offer's title is formatted to contain the type between []
-
-        :param str offer_clean_title: clean title
-        """
-        # clean the title: excluding text out of brackets
-        try:
-            contract = offer_clean_title.split("[")[1].split("]")[0]
-        except IndexError:
-            logging.warning(
-                "Title bad formatted. Offer RSS ID: {}".format(self.offer_id)
-            )
-            contract = offer_clean_title.split("]")[0].lstrip("[")
-
-        logging.debug("Contract extracted from title: {}".format(contract))
-        contract = contract.lower()
-
-        # find a contract match
-        if Contract.objects.filter(abbrv=contract).exists():
-            return Contract.objects.get(label=contract)
-        elif ContractVariations.objects.filter(label=contract).exists():
-            contract_var = ContractVariations.objects.get(label=contract).name
-            return Contract.objects.get(abbrv=contract_var)
-        else:
-            return Contract.objects.get(abbrv="ND")
-
-    def parse_place(self, offer_raw_title: str, mode: int = 0):
-        """
-        Extraction of types of contracts: CDI, CDD, mission, volontariat, etc.
-        In theory, place information is wihtin parenthesis '()'.
-
-        :param str offer_raw_title: offer title
-        :param int mode: 0 = STRICT regex (default): only digits between ()
-                         1 = MEDIUM regex: alphanumeric between ()
-                         2 = SOFT regex: alphanumeric code outside ()
-        """
-        # removing contract type between []
-        try:
-            title = offer_raw_title.split("[")[1].split("]")[1]
-            logging.debug("Title without contract: {}".format(title))
-        except IndexError:
-            logging.error("Title bad formatted. Offer RSS ID: {}".format(self.offer_id))
-            title = offer_raw_title
-
-        # extract with regex
-        if not mode:
-            dpt_code = re.findall("\((\d+)\)", title)
-            logging.debug("STRICT regex applied: {}".format(dpt_code))
-        elif mode == 1:
-            dpt_code = re.findall("\((2[AB]|[0-9]+)\)", title)
-            logging.debug("MEDIUM regex applied: {}".format(dpt_code))
-        elif mode == 2:
-            dpt_code = re.findall("(2[AB]|[0-9]+)", title)
-            logging.debug("SOFT regex applied: {}".format(dpt_code))
-        else:
-            raise TypeError("'mode' parameter only accepts an integer [0-2]")
-
-        # match French department code
-        if len(dpt_code) == 1:
-            if Place.objects.filter(code=dpt_code[0]).exists():
-                place_name = Place.objects.get(code=dpt_code[0]).name
-                logging.debug("Place code MATCHED in title: {}".format(dpt_code))
-                return Place.objects.get(name=place_name)
-            else:
-                logging.debug("Place code MATCHED in title: {}".format(title))
-                # try again
-                if mode < 2:
-                    return self.parse_place(title, mode=mode + 1)
-                else:
-                    pass
-        elif len(dpt_code) > 1:
-            logging.warning(
-                "More than possible department code found: {}.".format(
-                    ";".join(dpt_code)
-                )
-            )
-            # try again
-            if mode < 2:
-                return self.parse_place(title, mode=mode + 1)
-            else:
-                pass
-        elif not len(dpt_code):
-            logging.warning(
-                "No place code found in title." " Trying to find a place anyway..."
-            )
-            t_place = title[title.find("(") + 1 : title.find(")")]
-            if "," in t_place:
-                t_place = t_place.lower().split(",")
-            else:
-                t_place = t_place.lower().split()
-                pass
-            # try to get a match in place variations
-            for i in t_place:
-                if PlaceVariations.objects.filter(label=i).exists():
-                    pv = PlaceVariations.objects.get(label=i).name
-                    logging.debug("Place found: {}".format(i))
-                    return Place.objects.get(name=pv)
-                else:
-                    logging.debug("No place found in: {}".format(i))
-            # try again
-            if mode < 2:
-                return self.parse_place(title, mode=mode + 1)
-            else:
-                logging.warning("No place found in title: {}".format(offer_raw_title))
-                pass
-
-        # method ending if no place found during various attempts
-        return "ND"
-
     def parse_words(self, offer_raw_content):
         """
         Extraction of words mentioned into the offers. The goal is to perform
