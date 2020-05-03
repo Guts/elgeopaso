f0541bf010c2d985469f787ce3b8a4f3bdc085c6
Julien <Guts@users.noreply.github.com>
2020-05-03 14:41:24 +0200

    Improve post-receive git hook

--- .deploy/git-hooks/post-receive.sh
+++ .deploy/git-hooks/post-receive.sh
@@ -1,45 +1,47 @@
-#!/bin/sh
+#!/bin/bash
 
-# Variables
-# --------------------------------------------------------
-
-# Folder where to deploy the application
+# The production directory
 WWW="/var/www/elgeopaso"
 
 # A temporary directory for deployment
 TMP="/srv/tmp/elgeopaso"
 
-# The Git local repository
+# The Git repo
 GIT="/srv/git/elgeopaso.git"
 
-# Folder where to store environment files
+# The Env  repo
 ENV="/srv/env/elgeopaso"
 
-# Operations
-# --------------------------------------------------------
-
 # Deploy the content to the temporary directory
-mkdir -p \$TMP
+mkdir -p $TMP
 git --work-tree=$TMP --git-dir=$GIT checkout -f
 
 # Copy the env variable to the temporary directory
-cp -a \$ENV/. \$TMP
+echo "GIT STEP - Copy '.env' file to the temp folder"
+cp -a $ENV/. $TMP
 
 # Build tasks
 cd $TMP
+# Remove useless files and folders
+rm -rf ./vscode
 
-# Replace the production directory content with the temporary content
-cd /
+# Replace the content of the production directory
+# with the temporary directory
+cd / || exit
+
+echo "GIT STEP - Clean final folder: $WWW"
 rm -rf $WWW
+
+echo "GIT STEP - Move new file structure from $TMP to $WWW"
 mv $TMP $WWW
 
-# Release tasks in production folder (start services, etc.)
-cd \$WWW
+# Release tasks
+cd $WWW || exit
 
 # Create virtualenv and update pip
 virtualenv -p /usr/bin/python3.7 .venv
 source ./.venv/bin/activate
-python -m pip install -U pip
+python -m pip install -U pip setuptools wheel
 
 # Install dependencies
 python -m pip install -U -r requirements/production.txt
@@ -47,3 +49,6 @@ python -m nltk.downloader punkt stopwords
 
 # Run migrations
 python manage.py migrate
+
+# Collect static
+python manage.py collectstatic --noinput
