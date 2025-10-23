df6337880511fd455490fcb20394558bc733ed59
Yves Jacolin <yjacolin@free.fr>
2025-10-23 13:38:59 +0200

    Fix loader in production

--- elgeopaso/settings/production.py
+++ elgeopaso/settings/production.py
@@ -89,15 +89,15 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # TEMPLATES
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/fr/2.2/ref/settings/#templates
-TEMPLATES[-1]["OPTIONS"]["loaders"] = [  # type: ignore[index] # noqa F405
-    (
-        "django.template.loaders.cached.Loader",
-        [
-            "django.template.loaders.filesystem.Loader",
-            "django.template.loaders.app_directories.Loader",
-        ],
-    )
-]
+# TEMPLATES[-1]["OPTIONS"]["loaders"] = [  # type: ignore[index] # noqa F405
+#    (
+#        "django.template.loaders.cached.Loader",
+#        [
+#            "django.template.loaders.filesystem.Loader",
+#            "django.template.loaders.app_directories.Loader",
+#        ],
+#    )
+# ]
 
 # EMAIL
 # ------------------------------------------------------------------------------
